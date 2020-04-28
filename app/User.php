6c8be2431ec6d7e61f0cdb218c45ddd992aa91c1
Andres <pipecardenast@hotmail.com>
2020-04-28 00:06:54 -0500

    Models initialized

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'name', 'email', 'password', 'api_token'
     ];
 
     /**
@@ -29,4 +29,8 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    public function accounts() {
+        return $this->hasMany('App\Account');
+    }
 }
